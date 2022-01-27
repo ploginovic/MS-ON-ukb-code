0f2e3eccec7c8397e49718e25f08c3a85f37445f
ploginovic <96141580+ploginovic@users.noreply.github.com>
2022-01-27 13:32:32 +0000

    updated so that QCed gwas summary is used

--- nonhla_plink_2201.sh
+++ nonhla_plink_2201.sh
@@ -7,7 +7,7 @@ cd /slade/home/pl450/MS_GRS_overall_1412/non_HLA_GRS
 for i in {1..22}; do
 	nice plink2 --bgen /slade/projects/Research_Project-MRC158833/UKBiobank/500K_Genetic_data/imputed_data/ukb_imp_chr${i}_v3.bgen \
 		--sample /slade/projects/Research_Project-MRC158833/UKBiobank/500K_Genetic_data/imputed_data/ukb9072_imp_autosomes.sample \
-		--score imsgc_gwas_discovery_p.000001 3 4 8 header cols=+scoresums list-variants \
+		--score imsgc_gwas_discovery_p.000001_qced 3 4 8 header cols=+scoresums list-variants \
 		--out nonhla_msgrs_chr${i}_22_01 \
 	; done
 
