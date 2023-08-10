59d259f2c2cd4ce8314ccec4e18e14807ed033a9
ploginovic <96141580+ploginovic@users.noreply.github.com>
2023-08-10 20:49:49 +0300

    Update stratifiedKM_plot.py
    typo fixed

--- python_files/stratifiedKM_plot.py
+++ python_files/stratifiedKM_plot.py
@@ -80,28 +80,28 @@ def plot_km(data, cut_off_dict, save_label='Unnamed', plot_label='', figsize=[6,
         kmf_firstq, kmf_secondq, kmf_thirdq = KaplanMeierFitter(), KaplanMeierFitter(), KaplanMeierFitter()
         models = [kmf_firstq, kmf_secondq, kmf_thirdq]
     elif cut_off_dict == cut_off_tertiles:
-        kmf_labels = ["1st tertile", "2nd tertile", "3rd tertile"]
-        kmf_firstq, kmf_secondq, kmf_thirdq = KaplanMeierFitter(), KaplanMeierFitter(), KaplanMeierFitter()
-        models = [kmf_firstq, kmf_secondq, kmf_thirdq]
+kmf_labels = ["1st tertile", "2nd tertile", "3rd tertile"]
+kmf_firstq, kmf_secondq, kmf_thirdq = KaplanMeierFitter(), KaplanMeierFitter(), KaplanMeierFitter()
+models = [kmf_firstq, kmf_secondq, kmf_thirdq]
     else:
         print("Cut off dictionary invalid")
         return
     
     # Create the plot with specified dimensions and resolution
-    plt.figure(figsize=figsize, dpi=300, facecolor=None)
+plt.figure(figsize=figsize, dpi=300, facecolor=None)
     
     # Plot Kaplan-Meier survival curves for each group using stratified data
     for (group, group_label, kmf_model, color) in zip(stratified_data, kmf_labels, models, colours):
-        kmf_model.fit(group.ON_to_MS_years, group.first_ON, label='{group}, n={i}'.format(
-            group=group_label, i=len(group)))
-        ax = kmf_model.plot(show_censors=True, censor_styles={'ms': 1.3, 'marker': 'x'}, linewidth=0.9, color=color, fontsize=7)
+kmf_model.fit(group.ON_to_MS_years, group.first_ON, label='{group}, n={i}'.format(
+group=group_label, i=len(group)))
+ax = kmf_model.plot(show_censors=True, censor_styles={'ms': 1.3, 'marker': 'x'}, linewidth=0.9, color=color, fontsize=7)
     
     # Set plot limits and labels
-    ax.set_xlim([0.0, 35.0])
-    ax.set_ylim([0.0, 1.0])
-    plt.title(plot_label, fontsize=7) 
-    plt.xlabel("MS-free survival in years", fontsize=7)
-    plt.ylabel("MS-free survival probability", fontsize=7)
+ax.set_xlim([0.0, 35.0])
+ax.set_ylim([0.0, 1.0])
+plt.title(plot_label, fontsize=7) 
+plt.xlabel("MS-free survival in years", fontsize=7)
+plt.ylabel("MS-free survival probability", fontsize=7)
     
     # Add at-risk counts to the plot if necessary
     if len(cut_off_dict) == 3:
@@ -110,61 +110,56 @@ def plot_km(data, cut_off_dict, save_label='Unnamed', plot_label='', figsize=[6,
         add_at_risk_counts(models[0], models[1], models[2], models[3], fontsize=5)
     
     # Adjust layout and save the plot as a PNG file
-    plt.tight_layout()
-    plt.savefig(create_png_label(save_label))
-    plt.show()
+plt.tight_layout()
+plt.savefig(create_png_label(save_label))
+plt.show()
     
     
 
-
-Here's the annotated explanation of the function proportion_ci:
-
-python
-Copy code
 def proportion_ci(data, cut_off_dict, stratify=False):
     """
-    Calculates proportions and their 95% confidence intervals using the normal approximation to the binomial distribution.
+ Calculates proportions and their 95% confidence intervals using the normal approximation to the binomial distribution.
+ 
+ Args:
+ data (list): A list of DataFrames containing the data to be analyzed.
+ cut_off_dict (dict): A dictionary with cut-off points for analysis.
+ stratify (bool): Whether to stratify the data (default is False).
+ 
+ Returns:
+ DataFrame: A DataFrame containing calculated proportions and confidence intervals for each group and time point.
+ """
     
-    Args:
-        data (list): A list of DataFrames containing the data to be analyzed.
-        cut_off_dict (dict): A dictionary with cut-off points for analysis.
-        stratify (bool): Whether to stratify the data (default is False).
-        
-    Returns:
-        DataFrame: A DataFrame containing calculated proportions and confidence intervals for each group and time point.
-    """
-    
-    mean_ci_df = pd.DataFrame()  # Initialize an empty DataFrame to store results
+mean_ci_df = pd.DataFrame() # Initialize an empty DataFrame to store results
     
     for y, i in enumerate(data):
         
-        for ts, timepoint in enumerate(time_list):  # Iterate over time points
+        for ts, timepoint in enumerate(time_list): # Iterate over time points
             
             if len(i.loc[(i.first_ON == 'MS-ON') & (i.ON_to_MS_years <= timepoint)]) != 0:
-                n = i.loc[(i.first_ON == 'MS-ON') & (i.ON_to_MS_years <= timepoint)].first_ON.value_counts()[0]
+n = i.loc[(i.first_ON == 'MS-ON') & (i.ON_to_MS_years <= timepoint)].first_ON.value_counts()[0]
             else:
-                n = 0
+n = 0
                 
-            p = float(n / len(i))  # Calculate the proportion
-            crit_z = 1.96  # Critical value for a 95% confidence interval
-            ci_95 = crit_z * ((p * (1 - p)) / len(i)) ** 0.5  # Calculate the confidence interval
+p = float(n / len(i)) # Calculate the proportion
+crit_z = 1.96 # Critical value for a 95% confidence interval
+ci_95 = crit_z * ((p * (1 - p)) / len(i)) ** 0.5 # Calculate the confidence interval
             
             if y == 0:
                 # Create a dictionary for the confidence interval values
-                ci_data = {
+ci_data = {
                     ("lower_ci" + str(timepoint)): round(p - ci_95, 3),
                     ("proportion" + str(timepoint)): round(p, 3),
                     ("upper_ci" + str(timepoint)): round(p + ci_95, 3)
                 }
                 
                 # Create a DataFrame for the current time point
-                ci_df = pd.DataFrame([ci_data])
-                mean_ci_df = pd.concat([mean_ci_df, ci_df], axis=1)
+ci_df = pd.DataFrame([ci_data])
+mean_ci_df = pd.concat([mean_ci_df, ci_df], axis=1)
             
             elif y > 0:
                 # Update the DataFrame for additional time points
-                mean_ci_df.loc[y, ("lower_ci" + str(timepoint))] = round(p - ci_95, 3)
-                mean_ci_df.loc[y, ("proportion" + str(timepoint))] = round(p, 3)
+mean_ci_df.loc[y, ("lower_ci" + str(timepoint))] = round(p - ci_95, 3)
+mean_ci_df.loc[y, ("proportion" + str(timepoint))] = round(p, 3)
                 mean_ci_df.loc[y, ("upper_ci" + str(timepoint))] = round(p + ci_95, 3)
         
     return mean_ci_df  # Return the DataFrame with calculated proportions and confidence intervals
